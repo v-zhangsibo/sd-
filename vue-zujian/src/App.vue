6046afc029ab1f6c13092e27d589cbc36c0e4a4e
github125294303 <125294303@qq.com>
2019-08-14 21:02:54 +0800

    手动封装一个icon组件

---
+++ vue-zujian/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Icon type="caidan"
+          :size="520"
+          color="skyblue"></Icon>
+    <Icon type="aixin"
+          :size="520"
+          color="pink"></Icon>
+  </div>
+</template>
+<script>
+import Icon from "./views/icon";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Icon
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ vue-zujian/src/views/icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <i :class="classes"
+     :style="styles"></i>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String, //类型
+      default: "", //默认
+      required: true
+    },
+    size: {
+      type: Number, //类型
+      default: 12 //默认
+    },
+    color: {
+      type: String,
+      default: "red"
+    }
+  },
+  computed: {
+    classes() {
+      return ["iconfont", `icon-${this.type}`];
+    },
+    styles() {
+      return { fontSize: `${this.size}px`, color: `${this.color}` };
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
